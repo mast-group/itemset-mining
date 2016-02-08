09711a6942f6180a456e847f137a7c4257dc9148
Jari <jaroslav.fowkes@ed.ac.uk>
2016-02-08 15:34:50 +0000

    Return correct top exclusive itemsets in eval

--- itemset-miner/src/main/java/itemsetmining/eval/ExclusiveItemsets.java
+++ itemset-miner/src/main/java/itemsetmining/eval/ExclusiveItemsets.java
@@ -2,6 +2,7 @@ package itemsetmining.eval;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -10,12 +11,10 @@ import java.util.Set;
 
 import org.apache.commons.io.FileUtils;
 
-import com.google.common.collect.Sets;
-
 import itemsetmining.itemset.Itemset;
 import itemsetmining.main.ItemsetMiningCore;
 
-public class TopItemsets {
+public class ExclusiveItemsets {
 
 	private static final int topN = 10;
 	private static final String baseDir = "/afs/inf.ed.ac.uk/user/j/jfowkes/Code/Itemsets/";
@@ -28,67 +27,65 @@ public class TopItemsets {
 
 		final Map<Itemset, Double> intItemsets = ItemsetMiningCore
 				.readIIMItemsets(new File(baseDir + "Logs/" + IIMlog));
-
 		final LinkedHashMap<Itemset, Double> krimpItemsets = StatisticalItemsetMining
 				.readKRIMPItemsets(new File(baseDir + "KRIMP/" + KRIMPlog));
-
 		final LinkedHashMap<Itemset, Double> mtvItemsets = StatisticalItemsetMining
 				.readMTVItemsets(new File(baseDir + "MTV/" + MTVlog));
 
-		final Set<Itemset> IIMnotMTVorKRIMP = Sets
-				.difference(Sets.difference(intItemsets.keySet(), mtvItemsets.keySet()), krimpItemsets.keySet());
-		final Set<Itemset> MTVnotIIMorKRIMP = Sets
-				.difference(Sets.difference(mtvItemsets.keySet(), intItemsets.keySet()), krimpItemsets.keySet());
-		final Set<Itemset> KRIMPnotIIMorMTV = Sets
-				.difference(Sets.difference(krimpItemsets.keySet(), intItemsets.keySet()), mtvItemsets.keySet());
+		final Set<Itemset> IIMnotMTVorKRIMP = getExclusiveItemsets(intItemsets.keySet(), mtvItemsets.keySet(),
+				krimpItemsets.keySet());
+		final Set<Itemset> MTVnotIIMorKRIMP = getExclusiveItemsets(mtvItemsets.keySet(), intItemsets.keySet(),
+				krimpItemsets.keySet());
+		final Set<Itemset> KRIMPnotIIMorMTV = getExclusiveItemsets(krimpItemsets.keySet(), intItemsets.keySet(),
+				mtvItemsets.keySet());
 
 		final List<String> dict = FileUtils.readLines(new File(baseDir + "Datasets/Abstracts/abstracts.dictionary"));
 
 		// Print top ten
-		int count = 0;
 		System.out.print("\n============= IIM not MTV/KRIMP =============\n");
-		for (final Entry<Itemset, Double> entry : intItemsets.entrySet()) {
-			final Itemset set = entry.getKey();
-			if (set.size() > 1 && IIMnotMTVorKRIMP.contains(set)) {
-				System.out.print(String.format("%s\tprob: %1.5f \tint: %1.5f %n", decode(entry.getKey(), dict),
-						entry.getValue(), Double.NaN));
-				count++;
-				if (count == topN)
-					break;
-			}
-		}
-		System.out.println();
-
-		// Print top ten
-		count = 0;
+		printTopExclusiveItemsets(intItemsets, IIMnotMTVorKRIMP, dict);
 		System.out.print("\n============= MTV not IIM/KRIMP =============\n");
-		for (final Entry<Itemset, Double> entry : intItemsets.entrySet()) {
-			final Itemset set = entry.getKey();
-			if (set.size() > 1 && MTVnotIIMorKRIMP.contains(set)) {
-				System.out.print(String.format("%s\tprob: %1.5f \tint: %1.5f %n", decode(entry.getKey(), dict),
-						entry.getValue(), Double.NaN));
-				count++;
-				if (count == topN)
-					break;
-			}
-		}
-		System.out.println();
-
-		// Print top ten
-		count = 0;
+		printTopExclusiveItemsets(mtvItemsets, MTVnotIIMorKRIMP, dict);
 		System.out.print("\n============= KRIMP not IIM/MTV =============\n");
-		for (final Entry<Itemset, Double> entry : intItemsets.entrySet()) {
+		printTopExclusiveItemsets(krimpItemsets, KRIMPnotIIMorMTV, dict);
+
+	}
+
+	/**
+	 * Set A \ B u C
+	 * <p>
+	 * Note: slow but Guava contains/Set.difference doesn't work here
+	 */
+	private static Set<Itemset> getExclusiveItemsets(final Set<Itemset> setA, final Set<Itemset> setB,
+			final Set<Itemset> setC) {
+		final Set<Itemset> exclItemsets = new HashSet<>();
+		outer: for (final Itemset itemsetA : setA) {
+			for (final Itemset itemsetB : setB) {
+				if (itemsetA.equals(itemsetB))
+					continue outer;
+			}
+			for (final Itemset itemsetC : setC) {
+				if (itemsetA.equals(itemsetC))
+					continue outer;
+			}
+			exclItemsets.add(itemsetA);
+		}
+		return exclItemsets;
+	}
+
+	private static void printTopExclusiveItemsets(final Map<Itemset, Double> itemsets,
+			final Set<Itemset> exclusiveItemsets, final List<String> dict) {
+		int count = 0;
+		for (final Entry<Itemset, Double> entry : itemsets.entrySet()) {
 			final Itemset set = entry.getKey();
-			if (set.size() > 1 && KRIMPnotIIMorMTV.contains(set)) {
-				System.out.print(String.format("%s\tprob: %1.5f \tint: %1.5f %n", decode(entry.getKey(), dict),
-						entry.getValue(), Double.NaN));
+			if (set.size() > 1 && exclusiveItemsets.contains(set)) {
+				System.out.print(String.format("%s\tprob: %1.5f %n", decode(entry.getKey(), dict), entry.getValue()));
 				count++;
 				if (count == topN)
 					break;
 			}
 		}
 		System.out.println();
-
 	}
 
 	private static String decode(final Itemset set, final List<String> dict) {
